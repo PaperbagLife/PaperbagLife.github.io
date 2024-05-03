66c53bc7127af44f0aca6427abf61dfc7b750295
Yunkun (Ricky) Lu <ricky@clockwork.io>
2024-05-03 22:45:15 +0000

    add danmaku pic and update naming

--- vue-version/src/util/consts.ts
+++ vue-version/src/util/consts.ts
@@ -1,8 +1,9 @@
-import coupletroubleUrl from '../assets/img/couple_trouble.png'
-import cvshooterUrl from '../assets/img/cv_shooter.png'
-import mobaUrl from '../assets/img/moba.png'
-import shopsimUrl from '../assets/img/Shopsim.png'
-import starrailUrl from '../assets/img/starrail.png'
+import coupletroubleImg from '../assets/img/couple_trouble.png'
+import cvshooterImg from '../assets/img/cv_shooter.png'
+import mobaImg from '../assets/img/moba.png'
+import danmakuImg from '../assets/img/danmaku.png'
+import shopsimImg from '../assets/img/Shopsim.png'
+import starrailImg from '../assets/img/starrail.png'
 
 export const COLUMN_FILL_RATIO=0.84
 
@@ -21,14 +22,14 @@ export const projectEntries: Project[] = [
     timeline: 'Sept 2021 - Oct 2021',
     description:
       'Inspired by my previous project that used unity navigation, I wanted to try recreating the basic structure of the popular game League of Legends. I created characters that attacks, casts projectiles, and navigates around the terrain.',
-    img: mobaUrl
+    img: mobaImg
   },
   {
     title: 'ShopSim - C#, Unity',
     timeline: 'Jan 2021',
     description:
       "I created a fully animated 3D animation/simulation of shoppers walking around a shop. I used Unity's navigation to make pathing calculated at real time. I used finalIK to make realistic animations for the shoppers' 3D model. The simulation records paths of each shopper when they choose a destination to walk towards.",
-    img: shopsimUrl,
+    img: shopsimImg,
     link: 'https://www.youtube.com/watch?v=bK8pCRX2u8I'
   },
   {
@@ -36,7 +37,7 @@ export const projectEntries: Project[] = [
     timeline: 'Feb 2020',
     description:
       'I lead a team of 4 in Tartan Hacks 2020 and we won the "Best Valentines Hack" award. I was the main programmer for the game system. I used OpenCV to implement a control system that detects two different colors. The players has to hold hands wearing rings to control the car and avoid obstacles. This was an interesting take on the valentine theme, so we stood out and won.',
-    img: coupletroubleUrl,
+    img: coupletroubleImg,
     link: 'https://www.youtube.com/watch?v=w7r6SNbJifU'
   },
   {
@@ -44,7 +45,7 @@ export const projectEntries: Project[] = [
     timeline: 'May 2019',
     description:
       "This was my 15-112 Project. It is a top-down space shooter game. I used OpenCV to implement a control system that detects a red color. The game uses a dynamic difficulty system that adjusts itself to the player's performance. The boss has various different attack patterns and a cool laser attack.",
-    img: cvshooterUrl,
+    img: cvshooterImg,
     link: 'https://www.youtube.com/watch?v=WvnU6LHCbh4'
   },
   {
@@ -52,7 +53,7 @@ export const projectEntries: Project[] = [
     timeline: 'Sept 2023 - Ongoing',
     description:
       'After I started working on UI for my job, I thought I could use my newfound knowledge to make games using Vue and Typescript. I have made several minigames on my personal website, you can see them under the Games section on this website. The screenshot above is a turn based game with animations using css.',
-    img: starrailUrl,
+    img: starrailImg,
     link: 'paperbaglife@github.io/#/games'
   },
   {
@@ -189,11 +190,13 @@ export const games: WebGameProject[] = [
   },
   {
     title: 'Danmaku',
+    img: danmakuImg,
     description: 'Tohou-like top-down bullet hell using canvas',
     route: '/games/danmaku'
   },
   {
     title: 'Star rail',
+    img: starrailImg,
     description: 'Turn based combat similar to Honkai Star Rail',
     route: '/games/starrail'
   }
