81a5e5e4bb2665055fb326381d069730437ab556
PaperbagLife <xiamiantiaodajidan@gmail.com>
2024-05-07 21:12:24 -0700

    update swiper

--- vue-version/src/views/components/SlideShowComponenet.vue
+++ vue-version/src/views/components/SlideShowComponenet.vue
@@ -3,26 +3,39 @@ import { computed, onMounted, ref } from 'vue'
 import type { Project } from '@/util/consts'
 
 const SCROLL_PROJECT_WIDTH = 300
-const props = defineProps<{
+defineProps<{
   projects: Project[]
 }>()
 </script>
 
 <template>
-  <swiper-container navigation="true" pagination="true" slides-per-view="1">
-    <swiper-slide
-      class="d-flex justify-content-center"
-      v-for="project in projects"
-      :key="project.title"
-    >
-      <img
-        :src="project.img"
-        class="px-2"
-        :width="SCROLL_PROJECT_WIDTH"
-        :height="SCROLL_PROJECT_WIDTH / 2"
-      />
+  <swiper-container
+    ref="swiperContainer"
+    loop="true"
+    navigation="true"
+    pagination="true"
+    slides-per-view="1"
+  >
+    <swiper-slide class="d-flex" v-for="project in projects" :key="project.title">
+      <div class="slide-wrapper col-12 justify-content-center">
+        <div class="row mx-auto">{{ project.title }}</div>
+        <img
+          :src="project.img"
+          class="row"
+          :width="SCROLL_PROJECT_WIDTH"
+          :height="SCROLL_PROJECT_WIDTH / 2"
+        />
+      </div>
     </swiper-slide>
   </swiper-container>
 </template>
 
-<style scoped></style>
+<style scoped>
+swiper-container {
+  --swiper-theme-color: #66ccff;
+  --swiper-pagination-bottom: 0;
+}
+.slide-wrapper {
+  display: grid;
+}
+</style>
--- vue-version/vite.config.ts
+++ vue-version/vite.config.ts
@@ -5,7 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all tags with a dash as custom elements
+          isCustomElement: (tag) => tag.startsWith('swiper')
+        }
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
