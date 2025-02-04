75900e848ac136d631a82eca3b8be07d7c194fd3
PaperbagLife <xiamiantiaodajidan@gmail.com>
2025-02-03 22:44:08 -0800

    new build

--- forceDeploy.sh
+++
@@ -1,3 +0,0 @@
-git subtree split --prefix vue-version/dist -b gh-pages-subtree
-git push git@github.com:PaperbagLife/PaperbagLife.github.io.git gh-pages-subtree:gh-pages --force
-git branch -D gh-pages-subtree
