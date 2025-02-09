3295937baa39f56a6b4642a75226dd1b71e44359
Ricky Lu <yunkunl@alumni.cmu.edu>
2025-02-08 16:07:54 -0800

    new build

--- vue-version/dist/assets/ThreeView-ec57cdb3.js
+++ vue-version/dist/assets/ThreeView-ec57cdb3.js
@@ -1,4 +1,4 @@
-import{d as hl,g as ul,k as dl,a as fl,e as pl,o as ml}from"./index-4cd5c84d.js";/**
+import{d as hl,g as ul,k as dl,a as fl,e as pl,o as ml}from"./index-54612b92.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
