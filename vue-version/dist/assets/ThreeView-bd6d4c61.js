af2de42185a0215b67a1db78a72c1f574ae854d4
PaperbagLife <xiamiantiaodajidan@gmail.com>
2025-05-15 00:03:47 -0700

    new build

--- vue-version/dist/assets/ThreeView-bd6d4c61.js
+++ vue-version/dist/assets/ThreeView-bd6d4c61.js
@@ -1,4 +1,4 @@
-import{d as hl,g as ul,k as dl,a as fl,e as pl,o as ml}from"./index-3ffceecb.js";/**
+import{d as hl,g as ul,k as dl,a as fl,e as pl,o as ml}from"./index-efc83ac9.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
